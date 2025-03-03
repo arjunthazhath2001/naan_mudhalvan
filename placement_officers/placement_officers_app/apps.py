df47f12d9add7af03c953173fb4c43164f801022
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2025-03-03 07:31:50 +0530

    apps created

---
+++ placement_officers/placement_officers_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlacementOfficersAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "placement_officers_app"
---
+++ placement_team/placement_team_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlacementTeamAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "placement_team_app"
